6b55ae3f573891743db898ab32872c7d42115fa9
yaasir cheekoory <yaasir1997@gmail.com>
2024-05-02 01:18:01 +0400

    revert back before changes on server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "webcup2024_production"
 
   config.action_mailer.perform_caching = false
-  
-  config.hosts << "devcrew.maurice.webcup.hodi.host"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
