59045d3d78fe8467ceacd78c29499bb3495ae3cb
Rous <rous0308@gmail.com>
2024-05-04 16:55:25 +0400

    add navbar and cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dqjdq06qv'
+  config.api_key = '185517885558468'
+  config.api_secret = 'q3L_Io4ebwjl7gwDe_EHpee0GfY'
+  config.secure = true # Enforces HTTPS
+  config.cdn_subdomain = true # Use subdomain for assets
+end
